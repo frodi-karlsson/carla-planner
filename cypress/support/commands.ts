dc425377a4cb8f40a97f6b448ca058c70f7c7c22
Fróði Karlsson <frodikarlsson@gmail.com>
2024-01-20 21:45:14 +0100

    i guess we use triple slash for now

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-import 'cypress'
+/// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
